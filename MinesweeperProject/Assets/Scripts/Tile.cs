7cf887f274228bf39c1f3583c75e39b2d012de88
Vinicius Civali <viniciuscivali@gmail.com>
2021-05-27 16:55:29 -0300

    Clues system

---
+++ MinesweeperProject/Assets/Scripts/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public enum TileKind
+    {
+        Blank,
+        Mine,
+        Clue
+    }
+
+    public TileKind tileKind = TileKind.Blank; 
+}
